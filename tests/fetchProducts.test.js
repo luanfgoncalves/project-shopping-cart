d44e73ba7d5ca39b15422714ee45d8089885746c
luanfgoncalves <luanfgoncalves@outlook.com>
2022-06-06 16:21:15 -0300

    Requisito 1 - adiciona primeiros testes a fetchProduct

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,19 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('se fetchProducts é uma função', async () => {
+    expect(typeof fetchProducts).toEqual('function');
+  });
+  test('se, com o argumento computador e teste se fetch foi chamada', async () => {
+    expect(typeof fetchProducts('computador')).toHaveBeenCalled(); // não funcionou 
+  });
+  test('se, ao chamar a função fetchProducts com o argumento computador, a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
+    expect(typeof fetchProducts('computador')).toHaveBeenCalled();
+  });
+  test('se, o retorno da função fetchProducts com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+    expect(typeof fetchProducts('computador')).toHaveBeenCalled();
+  });
+  test('se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url.', async () => {
+    expect(typeof fetchProducts('computador')).toHaveBeenCalled();
+  });
 });
