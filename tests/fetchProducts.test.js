4ce307bc7e2926af55001851e72628921bec869a
luanfgoncalves <luanfgoncalves@outlook.com>
2022-06-06 16:36:22 -0300

    Requisito 1 - adiciona primeiros testes a fetchProduct(2)

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,16 +6,25 @@ describe('1 - Teste a função fetchProducts', () => {
   test('se fetchProducts é uma função', async () => {
     expect(typeof fetchProducts).toEqual('function');
   });
+
   test('se, com o argumento computador e teste se fetch foi chamada', async () => {
-    expect(typeof fetchProducts('computador')).toHaveBeenCalled(); // não funcionou 
+    fetchProducts('computador') // 1- chame a função, para o fetch poder capturar o que foi chamado.
+    expect(fetchProducts('computador')).toHaveBeenCalled(); // funcionou
   });
+
   test('se, ao chamar a função fetchProducts com o argumento computador, a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
-    expect(typeof fetchProducts('computador')).toHaveBeenCalled();
+    const expected = 'https://api.mercadolibre.com/sites/MLB/search?q=computador' // define o endpoint
+    fetchProducts('computador') // chama a função pro fetch poder pegar
+    expect(fetchProducts('computador')).toHaveBeenCalledWith(expected); // verificar o argumento da função
   });
+
   test('se, o retorno da função fetchProducts com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
-    expect(typeof fetchProducts('computador')).toHaveBeenCalled();
+    expect(fetchProducts('computador')).toHaveBeenCalled();
   });
+
   test('se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url.', async () => {
-    expect(typeof fetchProducts('computador')).toHaveBeenCalled();
+    expect(fetchProducts('computador')).toHaveBeenCalled();
   });
 });
+
+// Referências: 
