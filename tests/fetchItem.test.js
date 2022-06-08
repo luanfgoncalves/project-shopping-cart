a8d9c77abf2f8a4d1baa43f54a2c0373e5b456a8
luanfgoncalves <luanfgoncalves@outlook.com>
2022-06-08 13:27:19 -0300

    adiciona primeiros testes a fetchItems

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,12 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
+  test('se se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toEqual('function');
+  });
+  test('se, com o argumento MLB1615760527 e teste se fetch foi chamada', async () => {
+    await fetchItems('MLB1615760527') // 1- chame a função, para o fetch poder capturar o que foi chamado.
+    expect(fetch).toHaveBeenCalled(); // tem que verificar se o fetch foi chamado, não a função em si.
+  });
   fail('Teste vazio');
 });
